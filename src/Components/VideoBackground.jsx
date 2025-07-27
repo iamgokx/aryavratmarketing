43b8749f9a96b509922b829abde509503bc9538a
Gokul <lekhwargokul84@gmail.com>
2025-07-27 23:12:06 +0530

    sites pages update

--- src/Components/VideoBackground.jsx
+++ src/Components/VideoBackground.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import styles from "../styles/VideoBackground.module.css";
-
+import videoBg from "/assets/videos/bgEffect.mp4"
 const VideoBackground = () => {
   return (
     <>
       <video autoPlay loop muted playsInline className={styles.bgvideo}>
-        <source src={"/public/assets/videos/bgEffect.mp4"} type="video/mp4" />
+        <source src={videoBg} type="video/mp4" />
 
       </video>
     </>
